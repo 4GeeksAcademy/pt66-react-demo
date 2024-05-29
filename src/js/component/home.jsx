b4265c8e079a985d1ed7e253558a9ac4691792ec
Shane B <dotfortun@users.noreply.github.com>
2024-05-29 23:02:50 +0000

    Started working with the spoonacular api.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
-import Calculator from "./Calculator";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/655847/information?apiKey=150b3d1fc44d4503a4808decd9346790`
+    );
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setRecipe(data);
+    }
+  }, []);
+
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="col-8 offset-2 d-flex flex-column justify-space-around">
-          <Calculator />
+          <ul>
+            {recipe.extendedIngredients?.map((ing, idx) => (
+              <li key={idx}>{ing.name}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
